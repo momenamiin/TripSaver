c89d3f29fedddf7df7a61ca736ddc31eaa4ec2ec
momenamiin <momenamiin@gmail.com>
2018-03-04 19:45:13 +0200

    Momen

--- app/src/main/java/com/momenamiin/udacity/tripsaver/SearchbarFragment.java
+++ app/src/main/java/com/momenamiin/udacity/tripsaver/SearchbarFragment.java
@@ -1,26 +1,36 @@
 package com.momenamiin.udacity.tripsaver;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.location.places.Place;
-import com.google.android.gms.location.places.ui.PlaceSelectionListener;
-import com.google.android.gms.location.places.ui.SupportPlaceAutocompleteFragment;
+import android.widget.AutoCompleteTextView;
 
 
-public class SearchbarFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class SearchbarFragment extends Fragment implements GoogleApiClient.OnConnectionFailedListener{
+
+    @BindView(R.id.input_search) AutoCompleteTextView autoCompleteTextView ;
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private PlaceAutocompleteAdapter mPlaceAutocompleteAdapter ;
+    private GeoDataClient mGeoDataClient ;
+    private static final LatLngBounds LAT_LNG_BOUNDS = new LatLngBounds(
+            new LatLng(-40,-168) , new LatLng(71,136)
+    );
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public SearchbarFragment() {
         // Required empty public constructor
@@ -39,33 +49,25 @@ public class SearchbarFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        SupportPlaceAutocompleteFragment autocompleteFragment = (SupportPlaceAutocompleteFragment)
-                getActivity().getSupportFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
-        if (autocompleteFragment != null ) {
-            autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
-                @Override
-                public void onPlaceSelected(Place place) {
-                    // TODO: Get info about the selected place.
-                    Log.i("Memo", "Place: " + place.getName());
-                }
 
-                @Override
-                public void onError(Status status) {
-                    // TODO: Handle the error.
-                    Log.i("Memo", "An error occurred: " + status);
-                }
-            });
-        }else {
-            Log.v("Memo" , "elseeeeeeeeee");
-        }
         }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        mGeoDataClient = Places.getGeoDataClient(getContext(), null);
+
+        mPlaceAutocompleteAdapter = new PlaceAutocompleteAdapter(getContext() ,mGeoDataClient, LAT_LNG_BOUNDS , null );
         // Inflate the layout for this fragment
         View rootview = inflater.inflate(R.layout.fragment_searchbar, container, false);
-
+        ButterKnife.bind(this , rootview);
+        autoCompleteTextView.setAdapter(mPlaceAutocompleteAdapter);
         return rootview ;
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
